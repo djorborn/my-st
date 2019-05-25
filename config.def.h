d582e4f0aa43a6629b126f752483a02112935ccc
Daniel Osborn <djorborn@gmail.com>
2019-05-25 14:44:36 -0400

    dracula colors

--- config.def.h
+++ config.def.h
@@ -84,52 +84,51 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-        /* 8 normal colors */
-       /*bk*/ "#23262E",
-       /*c*/ "#00E8C6",
-       /*g*/ "#96E072",
-       /*m*/ "#C74DED",
-       /*b*/ "#7CB7FF",
-       /*y*/ "#FFE66D",
-       /*r*/ "#EE5D43",
-       /*w*/ "#D5CED9",
+       [0] = "#000000", /* black   */
+       [1] = "#ff5555", /* red     */
+       [2] = "#50fa7b", /* green   */
+       [3] = "#f1fa8c", /* yellow  */
+       [4] = "#bd93f9", /* blue    */
+       [5] = "#ff79c6", /* magenta */
+       [6] = "#8be9fd", /* cyan    */
+       [7] = "#bbbbbb", /* white   */
+     
+       /* 8 bright colors */
+       [8]  = "#44475a", /* black   */
+       [9]  = "#ff5555", /* red     */
+       [10] = "#50fa7b", /* green   */
+       [11] = "#f1fa8c", /* yellow  */
+       [12] = "#bd93f9", /* blue    */
+       [13] = "#ff79c6", /* magenta */
+       [14] = "#8be9fd", /* cyan    */
+       [15] = "#ffffff", /* white   */
+     
+       /* special colors */
+       [256] = "#282a36", /* background */
+       [257] = "#f8f8f2", /* foreground */
 
-        /* 8 bright colors */
-/*        "#303541",
-        "#9BC53D",
-        "#3B79C7", black, red, 
-        "#FF9F2E",
-        "#00e8c5",
-        "#eeeeee",
-        "#ff00aa",
-        "#f92672",
-*/
-    /*black*/  "#43464E",
-        /*c*/   "#30f8f6",
-    /*g*/   "#c6ffa2",
-        /*m*/   "#f77DfD",
-    /*b*/   "#aCe7FF",
-        /*y*/   "#FFf68D",
-        /*r*/   "#ff7D63",
-    /*w*/   "#f5eEe9",
-
-        [255] = 0,
-
-        /* more colors can be added after 255 to use with DefaultXX */
-        "#cccccc",
-        "#555555",
 };
 
 
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+
+ /*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+
+unsigned int defaultitalic = 7;
+unsigned int defaultunderline = 7;
+
 
 /*
  * Default shape of cursor
